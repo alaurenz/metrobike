1e14dbfe3014d34363a1fb19e2b359af628d237c
dutchscout <dutchscout@gmail.com>
2013-04-15 15:59:43 -0700

    The app now contacts Google Maps and displays the response as raw JSON code (large)

--- HelloMetroBike/src/com/HuskySoft/hellometrobike/MainActivity.java
+++ HelloMetroBike/src/com/HuskySoft/hellometrobike/MainActivity.java
@@ -1,5 +1,12 @@
 package com.HuskySoft.hellometrobike;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -36,6 +43,8 @@ public class MainActivity extends Activity {
 	}
 
 	private class GMapsCommTest implements Runnable {
+		String myResponse = "empty";
+		
 		@Override
 		public void run() {
 			// Change current text
@@ -46,15 +55,41 @@ public class MainActivity extends Activity {
 			});
 			
 			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// Do nothing if interrupted
+				URL url = new URL("http://maps.googleapis.com/maps/api/directions/xml?origin=Seattle%2CWA&destination=New%20York%2CNY&sensor=false");
+				//URL url = new URL("http://google.com");
+				HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+				try {
+					InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+					byte[] thisResponse = new byte[2048];
+					int bytesThisTime = 0;
+					do{
+						bytesThisTime = in.read(thisResponse);
+						if(bytesThisTime < 0) break;
+						String stringResponse = new String(thisResponse,0,bytesThisTime);
+						myResponse += stringResponse;
+					} while(bytesThisTime > 0);
+				} finally {
+					urlConnection.disconnect();
+				}
+				
+			} catch (MalformedURLException e) {
+				Log.e(TAG,"Bad url..." + e.getMessage());
+			} catch (IOException e) {
+				Log.e(TAG,"IO Exception..." + e.getMessage());
 			}
+			
+			Log.w(TAG, "Got response: " + myResponse);
+			
+//			try {
+//				Thread.sleep(2000);
+//			} catch (InterruptedException e) {
+//				// Do nothing if interrupted
+//			}
 			
 			// Change current text
 			MainActivity.this.runOnUiThread(new Runnable () {
 				public void run(){
-					mainText.setText("Got information!\n........................................");
+					mainText.setText("Got information!\n" + GMapsCommTest.this.myResponse);
 				}
 			});
 		}
