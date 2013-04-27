45db97803bbf8b22522a0877c6836a3e82b630cf
U-mengwan-PC\mengwan <mengwan@mengwan-PC.(none)>
2013-04-27 09:36:42 -0700

    add activity details.xml

---
+++ MetroBike/src/com/HuskySoft/metrobike/ui/DetailsActivity.java
@@ -0,0 +1,23 @@
+package com.HuskySoft.metrobike.ui;
+
+import com.HuskySoft.metrobike.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DetailsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_details);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_details, menu);
+		return true;
+	}
+
+}
