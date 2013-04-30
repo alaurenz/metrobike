7b5aff57aa97fe1c243ec313d384347462d2632a
U-mengwan-PC\mengwan <mengwan@mengwan-PC.(none)>
2013-04-30 16:24:13 -0700

    updating pictures and set the code to the right file

--- MetroBike/src/com/HuskySoft/metrobike/ui/NevigateActivity.java
+++ MetroBike/src/com/HuskySoft/metrobike/ui/NevigateActivity.java
@@ -1,30 +1,26 @@
 package com.HuskySoft.metrobike.ui;
 
 import com.HuskySoft.metrobike.R;
+import com.HuskySoft.metrobike.R.layout;
+import com.HuskySoft.metrobike.R.menu;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
-public class DetailsActivity extends Activity {
+public class NevigateActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_results);
+		setContentView(R.layout.activity_nevigate);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_results, menu);
+		getMenuInflater().inflate(R.menu.activity_nevigate, menu);
 		return true;
 	}
 
-	public void goToNevigate(View view) {
-        // Do something in response to button
-    	Intent intent = new Intent(this, NevigateActivity.class);
-    	startActivity(intent);
-    }
 }
--- MetroBike/src/com/HuskySoft/metrobike/ui/ResultsActivity.java
+++ MetroBike/src/com/HuskySoft/metrobike/ui/ResultsActivity.java
@@ -1,30 +1,24 @@
+
 package com.HuskySoft.metrobike.ui;
 
 import com.HuskySoft.metrobike.R;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
 public class ResultsActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_results);
+		setContentView(R.layout.activity_details);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_results, menu);
+		getMenuInflater().inflate(R.menu.activity_details, menu);
 		return true;
 	}
 
-	public void goToDetail(View view) {
-        // Do something in response to button
-    	Intent intent = new Intent(this, DetailsActivity.class);
-    	startActivity(intent);
-    }
 }
