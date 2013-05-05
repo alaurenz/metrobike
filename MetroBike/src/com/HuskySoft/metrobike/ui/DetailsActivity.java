f804da39204c22541e9b6c12945dfbb6e3971d52
dutchscout <dutchscout@gmail.com>
2013-05-05 00:55:00 -0700

    On behalf of smile76054789.  Added Details activity

--- MetroBike/src/com/HuskySoft/metrobike/ui/DetailsActivity.java
+++ MetroBike/src/com/HuskySoft/metrobike/ui/DetailsActivity.java
@@ -1,5 +1,9 @@
 package com.HuskySoft.metrobike.ui;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
@@ -7,111 +11,136 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.HuskySoft.metrobike.R;
+import com.HuskySoft.metrobike.backend.Leg;
+import com.HuskySoft.metrobike.backend.Route;
+import com.HuskySoft.metrobike.backend.Step;
 
 /**
- * @author mengwan
- * 
+ * @author mengwan, Xinyun Chen
+ *
  */
 public class DetailsActivity extends Activity {
 
-    /**
-     * onCreate function of DetailsActivity class Display the details of
-     * metroBike search.
-     * 
-     * {@inheritDoc}
-     * 
-     * @see android.app.Activity#onCreate(android.os.Bundle)
+	/**
+     * Results from the search.
      */
-    @Override
-    protected final void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.setDetails();
-        ActionBar actionBar = this.getActionBar();
-        actionBar.setTitle("Details");
-        setContentView(R.layout.activity_details);
-    }
+    private ArrayList<Route> routes = null;
 
     /**
-     * Show the menu bar when the setting button is clicked.
-     * 
-     * @param menu
-     *            The options menu in which you place your items.
-     * @return true if the menu to be displayed.
-     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     * Current route that should be displayed on the map.
      */
-    @Override
-    public final boolean onCreateOptionsMenu(final Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_menu, menu);
-        return true;
-    }
-
-    /**
-     * this method will be called when user click buttons in the setting menu.
-     * 
-     * @param item
-     *            the menu item that user will click
-     * @return true if user select an item
-     */
-    @Override
-    public final boolean onOptionsItemSelected(final MenuItem item) {
-        switch (item.getItemId()) {
-        case R.id.action_settings:
-            // user click the setting button, start the settings activity
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
+    private int currRoute = -1;
+    private TextView directions;
+    private TextView start;
+    private TextView destnation;
+	
+	/**
+	 * 
+	 * onCreate function of DetailsActivity class
+	 * Display the details of metroBike search
+	 * 
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_details);
+		directions = (TextView) findViewById(R.id.directions);
+		start = (TextView) findViewById(R.id.directionsStart);
+		destnation = (TextView) findViewById(R.id.directionsDest);
+		ActionBar actionBar = this.getActionBar();
+		actionBar.setTitle("Details");
+		@SuppressWarnings("unchecked")
+		List<Route> recievedRoutes = (ArrayList<Route>) getIntent()
+                .getSerializableExtra("List of Routes");
+        if (recievedRoutes != null) {
+            routes = (ArrayList<Route>) recievedRoutes;
+            currRoute = (Integer) getIntent().
+            getSerializableExtra("Current Route Index");
         }
-    }
-
-    /**
-     * Direct to navigate page.
+        this.setDetails();
+	}
+	
+	/**
+	 * 
+	 * onCreate menu option of DetailsActivity
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_menu, menu);
+		return true;
+	}
+	
+	 /**
+     * Invoked when user click setting button in the menu.
      * 
-     * @param view
-     *            : the view of the button onClick function of the go to
-     *            Navigate button
+     * @param menuItem
+     *            the items in the menu bar
      */
-    public final void goToNavigate(final View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, NavigateActivity.class);
+    public final void goToSettingsPage(final MenuItem menuItem) {
+        // start the settings activity
+        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 
-    /**
-     * Direct to search page.
-     * 
-     * @param view
-     *            the view of the button onClick function of the return to
-     *            search page button
-     */
-    public final void goToSearchPage(final View view) {
+	/**
+	 * 
+	 * @param view: the view of the button
+	 * onClick function of the go to Navigate button
+	 */
+	public void goToNavigate(View view) {
         // Do something in response to button
-        Intent intent = new Intent(this, SearchActivity.class);
-        startActivity(intent);
+    	Intent intent = new Intent(this, NavigateActivity.class);
+    	intent.putExtra("List of Routes", (Serializable) routes);
+        intent.putExtra("Current Route Index", currRoute);
+    	startActivity(intent);
     }
-
-    /**
-     * Direct to result page.
-     * 
-     * @param view
-     *            the view of the button onClick function for the return to
-     *            result page button
-     */
-
-    public final void goToResults(final View view) {
+	
+	/**
+	 * 
+	 * @param view: the view of the button
+	 * onClick function of the return to search page button
+	 */
+	public void goToSearchPage(View view) {
         // Do something in response to button
-        Intent intent = new Intent(this, ResultsActivity.class);
-        startActivity(intent);
+    	Intent intent = new Intent(this, SearchActivity.class);
+    	startActivity(intent);
     }
-
-    /**
-     * helper function that sets up the details of a route.
-     */
-    private void setDetails() {
-        // TODO sets up details
-    }
+	
+	/**
+	 * 
+	 * @param view: the view of the button
+	 * onClick function for the return to result page button
+	 */
+	
+	public void goToResults(View view) {
+		// Do something in response to button
+    	Intent intent = new Intent(this, ResultsActivity.class);
+    	intent.putExtra("List of Routes", (Serializable) routes);
+        intent.putExtra("Current Route Index", currRoute);
+    	startActivity(intent);
+	}
+	
+	/**
+	 * 
+	 * helper function that sets up the details of a route
+	 */
+	private void setDetails() {
+		//sets up details
+		Route displayRoute = routes.get(currRoute);
+		List<Leg> legs = displayRoute.getLegList();
+		for (int i = 0; i < legs.size(); i++) {
+			Leg curLeg = legs.get(i);
+			List<Step> steps = curLeg.getStepList();
+			start.append(legs.get(i).getStartAddress());
+			for (int j = 0; j < steps.size(); j++) {
+				Step s = steps.get(j);
+				directions.append("\nStep " + (j+1) + "   " + s.getHtmlInstruction());
+			}
+			destnation.append(legs.get(i).getEndAddress());
+		}
+	}
 }
