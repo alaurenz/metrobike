1a5783832b8825c38678e4f05a65797db6a8e2bc
sw11 <1samw1@gmail.com>
2013-05-19 20:59:03 -0700

    fix the wrong import of the historytest

--- MetroBike/test/src/com/HuskySoft/metrobike/ui/utility/test/HistoryTest.java
+++ MetroBike/test/src/com/HuskySoft/metrobike/ui/utility/test/HistoryTest.java
@@ -1,23 +1,17 @@
 package com.HuskySoft.metrobike.ui.utility.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
+import junit.framework.Assert;
+import junit.framework.TestCase;
 import com.HuskySoft.metrobike.ui.utility.History;
 
 /**
  * JUnit test class to test History class.
+ * Noted that wrong junit test import before.
  * 
  * @author Sam Wilson
  * 
  */
-public class HistoryTest {
-	/**
-	 * The default timeout time 3s.
-	 */
-	private static final int TIMEOUT = 3000;
+public class HistoryTest extends TestCase {
 
 	/**
 	 * Dump value to test.
@@ -39,33 +33,22 @@ public class HistoryTest {
 	/**
 	 * Black box test: test this class is singleton or not.
 	 */
-	@Test(timeout = TIMEOUT)
+	//@Test(timeout = TIMEOUT)
 	public final void testGetInstance() {
 		History firstHistoryObject = History.getInstance();
 		History secondHistoryObject = History.getInstance();
-		assertTrue("Get the History Instacne twice",
-				firstHistoryObject.equals(secondHistoryObject));
-	}
-
-	/**
-	 * White box test: test can it get the list of history.
-	 */
-	@Test(timeout = TIMEOUT, expected = UnsupportedOperationException.class)
-	public final void testGetHistoryReturnUnmodifityList() {
-		setup();
-		// this should throw exception
-		history.getHistory().add(TEST_STRING);
+		Assert.assertTrue(firstHistoryObject.equals(secondHistoryObject));
 	}
 
 	/**
 	 * White box test: test addAddress(String address).
 	 */
-	@Test(timeout = TIMEOUT)
+//	@Test(timeout = TIMEOUT)
 	public final void testAddSingleAddress01() {
 		setup();
 		// add a single address
 		history.addAddress(TEST_STRING);
-		assertTrue("Add single address",
+		Assert.assertTrue("Add single address",
 				history.getHistory().contains(TEST_STRING));
 		history.deleteAddress(TEST_STRING);
 	}
@@ -73,12 +56,12 @@ public class HistoryTest {
 	/**
 	 * White box test: test addAddress(String address).
 	 */
-	@Test(timeout = TIMEOUT)
+//	@Test(timeout = TIMEOUT)
 	public final void testAddSingleEmptyAddress02() {
 		setup();
 		// add a single address
 		history.addAddress("");
-		assertTrue("Add single empty address", history.getHistory()
+		Assert.assertTrue("Add single empty address", history.getHistory()
 				.contains(""));
 		history.deleteAddress("");
 	}
@@ -86,13 +69,13 @@ public class HistoryTest {
 	/**
 	 * White box test: test addAddress(String address).
 	 */
-	@Test(timeout = TIMEOUT)
+//	@Test(timeout = TIMEOUT)
 	public final void testAddSingleDuplicateAddress03() {
 		setup();
 		// add 2 address
 		history.addAddress(TEST_STRING);
 		history.addAddress(TEST_STRING);
-		assertTrue("Add single duplicate address", history.getHistory()
+		Assert.assertTrue("Add single duplicate address", history.getHistory()
 				.contains(TEST_STRING));
 		history.deleteAddress(TEST_STRING);
 	}
@@ -100,20 +83,20 @@ public class HistoryTest {
 	/**
 	 * White box test: test addAddress(String address).
 	 */
-	@Test(timeout = TIMEOUT)
+//	@Test(timeout = TIMEOUT)
 	public final void testAddSingleNullAddress04() {
 		setup();
 		String str = null;
 		// add a single address
 		history.addAddress(str);
-		assertTrue("Add null address", history.getHistory().contains(str));
+		Assert.assertTrue("Add null address", history.getHistory().contains(str));
 		history.deleteAddress(str);
 	}
 
 	/**
 	 * White box test: test addAddress(String address).
 	 */
-	@Test(timeout = TIMEOUT)
+//	@Test(timeout = TIMEOUT)
 	public final void testAddSingleDuplicateNullAddress04() {
 		setup();
 		String str = null;
@@ -121,28 +104,28 @@ public class HistoryTest {
 		history.addAddress(str);
 		history.addAddress(str);
 		// should not be crash
-		assertTrue("Add 2 null address", history.getHistory().contains(str));
+		Assert.assertTrue("Add 2 null address", history.getHistory().contains(str));
 		history.deleteAddress(str);
 	}
 
 	/**
 	 * White box test: test addAddress(String[] address).
 	 */
-	@Test(timeout = TIMEOUT)
+//	@Test(timeout = TIMEOUT)
 	public final void testAddArrayOfAddress01() {
 		setup();
 		String[] strArray = { "a", "b", "c", "d", "e" };
 		// add array address
 		history.addAddress(strArray);
 		// should not be crash
-		assertTrue(history.getHistory().contains(strArray[0]));
+		Assert.assertTrue(history.getHistory().contains(strArray[0]));
 		deleteArrayAfterTesting(strArray);
 	}
 
 	/**
 	 * White box test: test addAddress(String[] address).
 	 */
-	@Test(timeout = TIMEOUT)
+//	@Test(timeout = TIMEOUT)
 	public final void testAddArrayOfAddress02() {
 		setup();
 		String[] strArray = { "a", "b", "c", "d", "e" };
@@ -150,28 +133,28 @@ public class HistoryTest {
 		// add array address
 		history.addAddress(strArray);
 		int actual = history.getSize();
-		assertEquals(expected, actual);
+		Assert.assertEquals(expected, actual);
 		deleteArrayAfterTesting(strArray);
 	}
 
 	/**
 	 * White box test: test addAddress(String[] address).
 	 */
-	@Test(timeout = TIMEOUT)
+//	@Test(timeout = TIMEOUT)
 	public final void testAddArrayOfDuplicateAddress03() {
 		setup();
 		String[] strArray = { "a", "a", "a", "a", "a" };
 		// add array address
 		history.addAddress(strArray);
 		// should not be crash
-		assertTrue(history.getHistory().contains(strArray[0]));
+		Assert.assertTrue(history.getHistory().contains(strArray[0]));
 		deleteArrayAfterTesting(strArray);
 	}
 
 	/**
 	 * White box test: test addAddress(String[] address).
 	 */
-	@Test(timeout = TIMEOUT)
+//	@Test(timeout = TIMEOUT)
 	public final void testAddArrayOfDuplicateAddress04() {
 		setup();
 		String[] strArray = { "a", "a", "a", "a", "a" };
@@ -179,23 +162,37 @@ public class HistoryTest {
 		// add array address
 		history.addAddress(strArray);
 		int actual = history.getSize();
-		assertEquals(expected, actual);
+		Assert.assertEquals(expected, actual);
 		deleteArrayAfterTesting(strArray);
 	}
 
 	/**
 	 * White box test: test getAddress(int index).
 	 */
-	@Test(timeout = TIMEOUT)
+//	@Test(timeout = TIMEOUT)
 	public final void testGetAddedAddress01() {
 		setup();
 		int index = history.getSize();
 		// add array address
 		history.addAddress(TEST_STRING);
 		String actual = history.getAddress(index);
-		assertEquals(TEST_STRING, actual);
+		Assert.assertEquals(TEST_STRING, actual);
 		history.deleteAddress(TEST_STRING);
 	}
+	
+	/**
+	 * White box test: test deleteAll().
+	 */
+//	@Test(timeout = TIMEOUT)
+	public final void testDeleteAllAddress01() {
+		setup();
+		// add array address
+		history.deleteAll();
+		int actual = history.getSize();
+		Assert.assertEquals(0, actual);
+	}
+	
+	
 
 	/**
 	 * helper method for deleting the string array that has been added.
