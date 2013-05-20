70bde7fe6ec10ac17111b877930806dde4d411d6
sw11 <1samw1@gmail.com>
2013-05-19 21:26:30 -0700

    checkstyle of UtilityTest

--- MetroBike/test/src/com/HuskySoft/metrobike/ui/utility/test/UtilityTest.java
+++ MetroBike/test/src/com/HuskySoft/metrobike/ui/utility/test/UtilityTest.java
@@ -22,100 +22,124 @@ import com.google.android.gms.maps.model.LatLng;
  * 
  */
 public class UtilityTest extends TestCase {
-    
-    /**
-     * WhiteBox: This test the case of converting a null Location to a LatLng.
-     */
-    public void test_convertNullLocationToLatLng() {
-        Location testLocation = null;//new Location(TEST_LATITUDE_ONE, TEST_LONGITUDE_ONE);
-        LatLng testLatLng = Utility.convertLocation(testLocation);
-        Assert.assertNull("LatLng from null Location should be null.",testLatLng);
-    }
-    
-    /**
-     * BlackBox test: Test if LatLng consists with the Location.
-     */
-    public void test_convertListOfLocation() {
-        List<Location> toTest = new ArrayList<Location>();
-        for(int i = 1; i <= 100 ; i ++) {
-            toTest.add(new Location (i * 1.0, (101 - i) * 1.0));
-        }
-        List<LatLng> result = Utility.convertLocationList(toTest);
-        Assert.assertEquals(100, result.size());
-        HashSet<LatLng> set = new HashSet<LatLng>();
-        for (int i = 0; i < 100; i ++) {
-            set.add(result.get(i));
-        }
-        for (int i = 0; i < 100; i ++) {
-            set.remove(new LatLng(toTest.get(i).getLatitude(), toTest.get(i).getLongitude()));
-        }
-        Assert.assertEquals(0, set.size());
-    }
-    
-    /**
-     * WhiteBox: This test the case of processing null list of locations
-     */
-    public void test_convertNullList() {
-        Assert.assertNull(Utility.convertLocationList(null));
-    }
-    
-    /**
-     * BlackBox: this test if the center is always within the route region
-     */
-    public void test_cameraCenter() {
-        DirectionsRequest request = new DirectionsRequest();
-        
-        String startAddress = "The Space Needle";
-        String endAddress = "University of Washington, Seattle";
-        request.setStartAddress(startAddress);
-        request.setEndAddress(endAddress);
-        request.setDepartureTime(4000000);
-        request.setTravelMode(TravelMode.MIXED);
-        request.setMinNumberBusTransfers(0);
-        request.setMaxNumberBusTransfers(5);
-        
-        DirectionsStatus expected = DirectionsStatus.REQUEST_SUCCESSFUL;
 
-        DirectionsStatus actual = request.doDummyRequest();
-        Assert.assertEquals(
-                "Actual status for request.doRequest() call was: " + actual.getMessage(), expected,
-                actual);
-        List<Route> routes = request.getSolutions();
-        for (Route r : routes) {
-            LatLng center = Utility.getCameraCenter(r);
-            Assert.assertTrue(center.latitude < r.getNeBound().getLatitude());
-            Assert.assertTrue(center.longitude < r.getNeBound().getLongitude());
-            Assert.assertTrue(center.latitude > r.getSwBound().getLatitude());
-            Assert.assertTrue(center.longitude > r.getSwBound().getLongitude());
-        }
-    }
-    
-    
-    /**
-     * BlackBox: this test if the cameraZoom level is legal
-     */
-    public void test_cameraZoomLevel() {
-        DirectionsRequest request = new DirectionsRequest();
-        
-        String startAddress = "The Space Needle";
-        String endAddress = "University of Washington, Seattle";
-        request.setStartAddress(startAddress);
-        request.setEndAddress(endAddress);
-        request.setDepartureTime(4000000);
-        request.setTravelMode(TravelMode.MIXED);
-        request.setMinNumberBusTransfers(0);
-        request.setMaxNumberBusTransfers(5);
-        
-        DirectionsStatus expected = DirectionsStatus.REQUEST_SUCCESSFUL;
+	/**
+	 * Fix value 1. Need this in order to pass the checkstyle.
+	 */
+	private static final int SIZE = 100;
+	
+	/**
+	 * Fix value 2. Need this in order to pass the checkstyle.
+	 */
+	private static final int D_TIME = 4000000;
+	
+	/**
+	 * Fix value 3. Need this in order to pass the checkstyle.
+	 */
+	private static final int BUS_TRSF = 5;
+	
+	/**
+	 * Fix value 4. Need this in order to pass the checkstyle.
+	 */
+	private static final float ZOOM_1 = 600f;
+	
+	/**
+	 * Fix value 4. Need this in order to pass the checkstyle.
+	 */
+	private static final float ZOOM_2 = 250f;
+	
+	/**
+	 * WhiteBox: This test the case of converting a null Location to a LatLng.
+	 */
+	public final void testConvertNullLocationToLatLng() {
+		Location testLocation = null;
+		LatLng testLatLng = Utility.convertLocation(testLocation);
+		Assert.assertNull("LatLng from null Location should be null.",
+				testLatLng);
+	}
 
-        DirectionsStatus actual = request.doDummyRequest();
-        Assert.assertEquals(
-                "Actual status for request.doRequest() call was: " + actual.getMessage(), expected,
-                actual);
-        List<Route> routes = request.getSolutions();
-        for (Route r : routes) {
-            float f = Utility.getCameraZoomLevel(r, 600f, 350f);
-            Assert.assertTrue(f > 0);
-        }
-    }
+	/**
+	 * BlackBox test: Test if LatLng consists with the Location.
+	 */
+	public final void testConvertListOfLocation() {
+		List<Location> toTest = new ArrayList<Location>();
+		for (int i = 1; i <= SIZE; i++) {
+			toTest.add(new Location(i * 1.0, (SIZE + 1 - i) * 1.0));
+		}
+		List<LatLng> result = Utility.convertLocationList(toTest);
+		Assert.assertEquals(SIZE, result.size());
+		HashSet<LatLng> set = new HashSet<LatLng>();
+		for (int i = 0; i < SIZE; i++) {
+			set.add(result.get(i));
+		}
+		for (int i = 0; i < SIZE; i++) {
+			set.remove(new LatLng(toTest.get(i).getLatitude(), toTest.get(i)
+					.getLongitude()));
+		}
+		Assert.assertEquals(0, set.size());
+	}
+
+	/**
+	 * WhiteBox: This test the case of processing null list of locations.
+	 */
+	public final void testConvertNullList() {
+		Assert.assertNull(Utility.convertLocationList(null));
+	}
+
+	/**
+	 * BlackBox: this test if the center is always within the route region.
+	 */
+	public final void testCameraCenter() {
+		DirectionsRequest request = new DirectionsRequest();
+
+		String startAddress = "The Space Needle";
+		String endAddress = "University of Washington, Seattle";
+		request.setStartAddress(startAddress);
+		request.setEndAddress(endAddress);
+		request.setDepartureTime(D_TIME);
+		request.setTravelMode(TravelMode.MIXED);
+		request.setMinNumberBusTransfers(0);
+		request.setMaxNumberBusTransfers(BUS_TRSF);
+
+		DirectionsStatus expected = DirectionsStatus.REQUEST_SUCCESSFUL;
+
+		DirectionsStatus actual = request.doDummyRequest();
+		Assert.assertEquals("Actual status for request.doRequest() call was: "
+				+ actual.getMessage(), expected, actual);
+		List<Route> routes = request.getSolutions();
+		for (Route r : routes) {
+			LatLng center = Utility.getCameraCenter(r);
+			Assert.assertTrue(center.latitude < r.getNeBound().getLatitude());
+			Assert.assertTrue(center.longitude < r.getNeBound().getLongitude());
+			Assert.assertTrue(center.latitude > r.getSwBound().getLatitude());
+			Assert.assertTrue(center.longitude > r.getSwBound().getLongitude());
+		}
+	}
+
+	/**
+	 * BlackBox: this test if the cameraZoom level is legal.
+	 */
+	public final void testCameraZoomLevel() {
+		DirectionsRequest request = new DirectionsRequest();
+
+		String startAddress = "The Space Needle";
+		String endAddress = "University of Washington, Seattle";
+		request.setStartAddress(startAddress);
+		request.setEndAddress(endAddress);
+		request.setDepartureTime(D_TIME);
+		request.setTravelMode(TravelMode.MIXED);
+		request.setMinNumberBusTransfers(0);
+		request.setMaxNumberBusTransfers(BUS_TRSF);
+
+		DirectionsStatus expected = DirectionsStatus.REQUEST_SUCCESSFUL;
+
+		DirectionsStatus actual = request.doDummyRequest();
+		Assert.assertEquals("Actual status for request.doRequest() call was: "
+				+ actual.getMessage(), expected, actual);
+		List<Route> routes = request.getSolutions();
+		for (Route r : routes) {
+			float f = Utility.getCameraZoomLevel(r, ZOOM_1, ZOOM_2);
+			Assert.assertTrue(f > 0);
+		}
+	}
 }
